a8a1c826b2eed75cf25cb219795ede81852bcaca
mtomus <mtomus.ext@talentia-software.com>
2024-07-23 17:01:25 +0300

    - add call time validators - add candiate model - add appSettings + launchSettings

---
+++ TestProject/TestProject/Models/Candidate.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TestProject.Validators;
+
+namespace TestProject.Models
+{
+    public class Candidate
+    {
+
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Comment is required")]
+        public string Comment { get; set; }
+        public string PhoneNumber { get; set; }
+        public string LinkedinUrl { get; set; }
+        public string GitHubUrl { get; set; }
+
+        public TimeSpan? CallTimeStart { get; set; }
+
+        [CallTimeEndValidation("CallTimeStart")]
+        public TimeSpan? CallTimeEnd { get; set; }
+    }
+}
